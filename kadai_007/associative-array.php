202df648385108a303c8a05cfb35bc3db6d9034e
yanari8 <yanari8co@gmail.com>
2025-01-18 21:49:40 +0900

    kadai_007 upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+        $vegetable = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($vegetable);
+        ?>
+    </p>
+</body>
+</html>
